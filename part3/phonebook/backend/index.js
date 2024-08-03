0a9355c9cf6fd34d16a4b6548e13474c62a0534c
Kamanpreet Singh <kskaman@gmail.com>
2024-08-03 11:31:22 -0230

    done 3.13-3.14

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const app = express();
 
 
+
 app.use(express.static('dist'))
 
 
@@ -12,28 +13,39 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      id: "1",
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: "2",
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: "3",
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: "4",
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-];
+
+
+
+require('dotenv').config()
+
+const Person = require('./models/person')
+
+
+
+
+
+// let persons = [
+//     { 
+//       id: "1",
+//       name: "Arto Hellas", 
+//       number: "040-123456"
+//     },
+//     { 
+//       id: "2",
+//       name: "Ada Lovelace", 
+//       number: "39-44-5323523"
+//     },
+//     { 
+//       id: "3",
+//       name: "Dan Abramov", 
+//       number: "12-43-234345"
+//     },
+//     { 
+//       id: "4",
+//       name: "Mary Poppendieck", 
+//       number: "39-23-6423122"
+//     }
+// ]
 
 
 const cors = require('cors')
@@ -42,40 +54,46 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {    
     response.json(persons);
-});
+  })
+})
 
 app.get('/info', (request, response) => {
-    const entryCount = persons.length;
-    const date = new Date();
-    response.send(
-        `<p>Phonebook has info for ${entryCount} people</p>
-         <p>${date}</p>`
-    );
-});
+    Person.countDocuments({})
+    .then(count => {
+        const date = new Date()
+        response.send(`
+            <p>Phonebook has info for ${count} people</p>
+            <p>${date}</p>
+        `)
+    })
+    .catch(error => {
+        console.error('Error fetching count : ', error)
+        response.status(500).send('<p>Error retreiving information from the database.</p>')
+    })
+})
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const person = persons.find(person => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
-});
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => response.status(404).end())
+})
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
-});
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => response.status(400).send(`<p>${error} encountered.</p>`))
+})
 
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(person => Number(person.id)))
-        : 0;
-    return String(maxId + 1);
-};
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
@@ -92,29 +110,32 @@ app.post('/api/persons', (request, response) => {
         });
     }
 
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        });
-    }
-
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    };
-
-    persons = persons.concat(person);
-    response.json(person);
-});
+    Person.findOne({name: name })
+    .then(existingName => {
+        if (existingName) {
+            return response.status(404).json({
+                error: 'Name must be unique'
+            })
+        } else {
+            const person = {
+                name: body.name,
+                number: body.number,
+            };
+        
+            Person.save().then(savedPerson => {
+                response.json(person)
+            })
+        }
+    })
+})
 
 const unknownEndPoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
-};
+}
 
 app.use(unknownEndPoint);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
